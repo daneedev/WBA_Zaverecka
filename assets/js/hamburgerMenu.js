7b9e59e5d4a7e25e5b1a708b8b4e4ee50cfbcfd9
daneedev <dan@danee.dev>
2025-04-01 20:35:50 +0200

    hamburgerMenu fix

--- assets/js/hamburgerMenu.js
+++ assets/js/hamburgerMenu.js
@@ -1,8 +1,8 @@
 function toggleMenu() {
     const menu = document.querySelector('.hamburger-menu');
-    if (menu.style.display === "none") {
-        menu.style.display = "flex";
-    } else {
+    if (menu.style.display === "flex") {
         menu.style.display = "none";
+    } else {
+        menu.style.display = "flex";
     }
 }
