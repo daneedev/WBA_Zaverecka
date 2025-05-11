f1198ed75b6f28334ddbd4dd578bc7d62179f46c
daneedev <dan@danee.dev>
2025-05-11 13:38:53 +0200

    js onclick fix

--- assets/js/hamburgerMenu.js
+++ assets/js/hamburgerMenu.js
@@ -1,3 +1,8 @@
+const hamburgerButton = document.querySelector('.hamburger-btn');
+const closeButton = document.querySelector('.hamburger-close');
+hamburgerButton.addEventListener('click', toggleMenu);
+closeButton.addEventListener('click', toggleMenu);
+
 function toggleMenu() {
     const menu = document.querySelector('.hamburger-menu');
     if (menu.style.display === "flex") {
